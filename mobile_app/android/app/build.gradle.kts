38f5175801d3e9820edb322e28f2b22a81b0a651
Freddy Calle <agz@MacBook-de-Freddy.local>
2026-06-24 23:45:45 -0400

    Fix camara Android: minSdk=21, uses-feature camera, launchMode singleTask para CameraX

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.cafelapaz.pos.coffe_shop_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion  // CameraX (mobile_scanner v7) requires min SDK 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
